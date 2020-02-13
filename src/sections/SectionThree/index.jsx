a18a517523bc169ccfeeb1da56409ffefb5c5e49
Sonny Schmidt <pgs.sonny@gmail.com>
2020-02-13 23:39:53 +0800

    Add clam svg

--- src/sections/SectionThree/index.jsx
+++ src/sections/SectionThree/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-// import styles from './styles.module.scss';
+import { ReactSVG } from 'react-svg';
+import styles from './styles.module.scss';
+import { ReactComponent as ClamTopSVG } from '../../images/clam-top.svg';
+import { ReactComponent as ClamBottomSVG } from '../../images/clam-bottom.svg';
 import {
   Section,
 } from '../../components';
@@ -8,7 +11,13 @@ const SectionThree = (props) => {
   return (
     <div className={`${props.className}`}>
       <Section>
-        SectionThree
+        <div className={styles.clamTopContainer}>
+          <ClamTopSVG />
+        </div>
+
+        <div className={styles.clamBottomContainer}>
+          <ClamBottomSVG />
+        </div>
       </Section>
     </div>
   );
